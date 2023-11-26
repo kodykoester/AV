c547d6ee68db18e680825b6e21ea2af94fde4045
kodykoester <105684121+kodykoester@users.noreply.github.com>
2023-11-25 19:11:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,55 +19,7 @@ int directionPinB = 13;
 int speedPinB = 11;
 int brakePinB = 8;
 
-int getDistance(int trigPin, int echoPin) {   // Generate a pulse for the trigger pin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH);  // Measure the duration of the echo pulse
-  int distance = duration * 0.034 / 2;  // Calculate the distance in centimeters
-  return distance;
-}
 
-// Function to make the robot go straight (move forward)
-void goStraight() {
-  digitalWrite(brakePinA, LOW);        // Release brakes
-  digitalWrite(brakePinB, LOW);        // Release brakes
-  digitalWrite(directionPinA, HIGH);   // Motor A forward
-  digitalWrite(directionPinB, LOW);   // Motor B forward
-
-  // Set speed for both motors
-  analogWrite(speedPinA, 170);  // Adjust the speed as needed (0 to 255)
-  analogWrite(speedPinB, 170);  // Adjust the speed as needed (0 to 255)
-}
-
-// Function to stop the motors
-void stopMotors() {
-  // Brake both motors
-  digitalWrite(brakePinA, HIGH);
-  digitalWrite(brakePinB, HIGH);
-}
-
-void turnRight() {
-  digitalWrite(directionPinA, HIGH);  // Set motor A to turn right
-  digitalWrite(speedPinA, 200);       // Set speed to maximum
-  digitalWrite(brakePinA, LOW);       // Disable brake for motor A
-
-  digitalWrite(directionPinB, LOW);   // Set motor B to turn right
-  digitalWrite(speedPinB, 50);       // Set speed to maximum
-  digitalWrite(brakePinB, LOW);       // Disable brake for motor B
-}
-
-void turnLeft() {
-  digitalWrite(directionPinA, LOW);   // Set motor A to turn left
-  digitalWrite(speedPinA, 50);       // Set speed to maximum
-  digitalWrite(brakePinA, LOW);       // Disable brake for motor A
-
-  digitalWrite(directionPinB, HIGH);  // Set motor B to turn left
-  digitalWrite(speedPinB, 200);       // Set speed to maximum
-  digitalWrite(brakePinB, LOW);       // Disable brake for motor B
-}
 
 enum RobotState {
   STATE_STOPPED,
@@ -107,41 +59,48 @@ int center = getDistance(trigPin1, echoPin1);
   int left = getDistance(trigPin2, echoPin2);
   int right = getDistance(trigPin3, echoPin3);
 
-  Serial.print("Left: ");
-  Serial.print(left);
-  Serial.print(" cm ");
+  // Serial.print("Left: ");
+  // Serial.print(left);
+  // Serial.print(" cm ");
 
-  Serial.print("Center: ");
-  Serial.print(center);
-  Serial.print(" cm ");
+  // Serial.print("Center: ");
+  // Serial.print(center);
+  // Serial.print(" cm ");
 
-  Serial.print("Right: ");
-  Serial.print(right);
-  Serial.println(" cm");
+  // Serial.print("Right: ");
+  // Serial.print(right);
+  // Serial.println(" cm");
 
   // Example: Switch states based on conditions
-  if (center < closeDistance) {
-    currentState = STATE_AVOID_OBSTACLE;
-  } else if (left < closeDistance) {
-    currentState = STATE_TURNING_RIGHT;
-  } else if (right < closeDistance) {
-    currentState = STATE_TURNING_LEFT;
-  } else {
-    currentState = STATE_GOING_STRAIGHT;
-  }
+if (center > closeDistance && left > closeDistance && right > closeDistance) {
+  currentState = STATE_GOING_STRAIGHT;
+} else if (center > closeDistance && left < closeDistance && right > closeDistance) {
+  currentState = STATE_TURNING_RIGHT;
+} else if (center > closeDistance && left > closeDistance && right < closeDistance) {
+  currentState = STATE_TURNING_LEFT;
+} else if (center < closeDistance) {
+  currentState = STATE_STOPPED;
+}
+  // } else {
+  //   currentState = STATE_GOING_STRAIGHT;
+  // }
 
   // Execute the appropriate function based on the current state
   switch (currentState) {
     case STATE_STOPPED:
+    Serial.print("Stop");
       stopMotors();
       break;
     case STATE_GOING_STRAIGHT:
+
       goStraight();
       break;
     case STATE_TURNING_RIGHT:
+    Serial.print("Right");
       turnRight();
       break;
     case STATE_TURNING_LEFT:
+    Serial.print("Left");
       turnLeft();
       break;
     case STATE_AVOID_OBSTACLE:
@@ -153,5 +112,56 @@ int center = getDistance(trigPin1, echoPin1);
       break;
   }
 
-  delay(100);
+  delay(10);
+}
+
+int getDistance(int trigPin, int echoPin) {   // Generate a pulse for the trigger pin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);  // Measure the duration of the echo pulse
+  int distance = duration * 0.034 / 2;  // Calculate the distance in centimeters
+  return distance;
+}
+
+// Function to make the robot go straight (move forward)
+void goStraight() {
+  digitalWrite(brakePinA, LOW);        // Release brakes
+  digitalWrite(brakePinB, LOW);        // Release brakes
+  digitalWrite(directionPinA, HIGH);   // Motor A forward
+  digitalWrite(directionPinB, LOW);   // Motor B forward
+
+  // Set speed for both motors
+  analogWrite(speedPinA, 150);  // Adjust the speed as needed (0 to 255)
+  analogWrite(speedPinB, 150);  // Adjust the speed as needed (0 to 255)
+}
+// Function to stop the motors
+void stopMotors() {
+  // Brake both motors
+  digitalWrite(brakePinA, HIGH);
+  digitalWrite(brakePinB, HIGH);
+}
+
+void turnRight() {
+  digitalWrite(directionPinA, HIGH);  // Set motor A to turn right
+  digitalWrite(speedPinA, 150);       // Set speed to maximum
+  digitalWrite(brakePinA, LOW);       // Disable brake for motor A
+  Serial.println("Faster A");
+  digitalWrite(directionPinB, LOW);   // Set motor B to turn right
+  digitalWrite(speedPinB, 0);       // Set speed to maximum
+  digitalWrite(brakePinB, HIGH);       // Disable brake for motor B
+  delay(10);
+}
+
+void turnLeft() {
+  digitalWrite(directionPinA, HIGH);   // Set motor A to turn left
+  digitalWrite(speedPinA, 0);       // Set speed to maximum
+  digitalWrite(brakePinA, HIGH);       // Disable brake for motor A
+Serial.println("Faster B");
+  digitalWrite(directionPinB, LOW);  // Set motor B to turn left
+  digitalWrite(speedPinB, 150);       // Set speed to maximum
+  digitalWrite(brakePinB, LOW);       // Disable brake for motor B
+  delay(10)
 }
